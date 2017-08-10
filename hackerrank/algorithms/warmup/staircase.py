0124e71fea5e4bed1a9229d4443f7be967d1077c
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-10 11:38:50 +0200

    Update staircase.py

--- hackerrank/algorithms/warmup/staircase.py
+++ hackerrank/algorithms/warmup/staircase.py
@@ -1,6 +1,10 @@
-#!/bin/python3
+n = int(input())
 
-import sys
-
-
-n = int(input().strip())
+for i in range(0, n):
+    for s in range(0, n-i-1):
+        print(' ', end='')
+        
+    for b in range(0, i+1):
+        print('#', end='')
+    
+    print ('')

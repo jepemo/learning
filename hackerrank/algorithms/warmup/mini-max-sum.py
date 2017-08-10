215d9d66db6754f91fe08041d11d0e3b1f63aa56
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-10 11:37:43 +0200

    Update mini-max-sum.py

--- hackerrank/algorithms/warmup/mini-max-sum.py
+++ hackerrank/algorithms/warmup/mini-max-sum.py
@@ -1,5 +1,11 @@
-#!/bin/python3
+#!/bin/python
 
 import sys
 
-arr = list(map(int, input().strip().split(' ')))
+
+a,b,c,d,e = input().strip().split(' ')
+a,b,c,d,e = [int(a),int(b),int(c),int(d),int(e)]
+
+v = sorted([a, b, c, d, e])
+
+print(sum(v[0:4]), sum(v[1:]))

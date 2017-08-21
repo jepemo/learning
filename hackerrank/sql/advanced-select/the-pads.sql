3a1df46cfe21a8d613e3b2abb7ce15a803df9c02
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-21 09:17:09 +0200

    Create the-pads.sql

---
+++ hackerrank/sql/advanced-select/the-pads.sql
@@ -0,0 +1,17 @@
+SELECT 
+    CONCAT(NAME, '(', SUBSTRING(OCCUPATION, 1, 1),')')
+FROM 
+    OCCUPATIONS 
+ORDER BY
+    NAME
+    ;
+    
+SELECT
+    CONCAT('There are a total of ', COUNT(*), ' ', LOWER(OCCUPATION), 's.')
+FROM 
+    OCCUPATIONS
+GROUP BY
+    OCCUPATION
+ORDER BY
+    COUNT(*), OCCUPATION
+    ;

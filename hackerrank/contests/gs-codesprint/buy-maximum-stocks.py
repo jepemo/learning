59c52844e6c099140d77688c66745fd4c26b8ae8
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-21 08:58:34 +0200

    Update buy-maximum-stocks.py

--- hackerrank/contests/gs-codesprint/buy-maximum-stocks.py
+++ hackerrank/contests/gs-codesprint/buy-maximum-stocks.py
@@ -2,19 +2,33 @@
 
 import sys, math
 
+def reorder_stocks(a):
+    values = []
+    current_day = 1
+    for stock in a:
+        values.append((current_day, stock))
+        current_day += 1
+        
+    values = sorted(values, key=lambda x: x[1])
+    
+    return values
+    
+
 def buyMaximumProducts(n, k, a):
     purchased_stocks = 0
-    current_day = 1
     spent = 0.0
-    for stock_price in a:
+    
+    stocks_days = reorder_stocks(a)
+    
+    for sd in stocks_days:
+        (current_day, stock_price) = sd
+        
         available_amount = k-spent
         allowed_buy = int(available_amount / stock_price)
         stocks_buy = allowed_buy if allowed_buy < current_day else current_day
 
         purchased_stocks += stocks_buy
         spent += float(stock_price * stocks_buy)
-
-        current_day += 1
 
     return purchased_stocks
 

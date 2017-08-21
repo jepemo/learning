9e38c8d07574717f546d2533afac66ea48c402e3
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-21 17:19:04 +0200

    Create if-else.py

---
+++ hackerrank/python/introduction/if-else.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 != 0:
+        print ("Weird")
+    else:    
+        if n in range(2, 6):
+            print ("Not Weird")
+        elif n in range(6, 21):
+            print ("Weird")
+        elif n > 20:
+            print ("Not Weird")

b8f8051480ff45c4feea9bfe1ad8f808690ac3ea
Jeremies Pérez <jeremiespm@gmail.com>
2017-08-28 14:29:43 +0200

    Create write-a-function.py

---
+++ hackerrank/python/introduction/write-a-function.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+    leap = False
+    
+    leap = (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))
+    
+    return leap
+    
+year = int(input())
+print(is_leap(year))

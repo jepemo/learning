428bc7ad92a20445eb077f4e7eab8f9ddbe3e30a
Jeremies Pérez <jeremiespm@gmail.com>
2017-09-07 17:04:00 +0200

    Update 01_pandas.py

--- datacamp/tutorials/finance-python-trading/01_pandas.py
+++ datacamp/tutorials/finance-python-trading/01_pandas.py
@@ -9,3 +9,15 @@ aapl.tail()
 
 # Describe `aapl`
 aapl.describe()
+
+# Inspect the index 
+aapl.index
+
+# Inspect the columns
+aapl.columns
+
+# Select only the last 10 observations of `Close`
+ts = aapl['Close'][-10:]
+
+# Check the type of `ts` 
+type(ts)

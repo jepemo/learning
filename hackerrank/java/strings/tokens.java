526fb1a01290ece4c13ec604e2e1809db491cc37
Jeremies Pérez <jeremiespm@gmail.com>
2018-03-01 14:19:41 +0100

    Update tokens.java

--- hackerrank/java/strings/tokens.java
+++ hackerrank/java/strings/tokens.java
@@ -6,13 +6,24 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        //String[] splitted = s.split("[A-Za-z !,\\?\\._'@]+");
-        String[] splitted = s.split("[ ',\\?!@\\._]+");
-        System.out.println(splitted.length);
-        for (String ss: splitted) {
-            System.out.println(ss);
+        
+        s = s.trim();
+        
+        if (s.length() > 400000) {
+            System.out.println("");
         }
-        // Write your code here.
+        else if (s.length() == 0) {
+            System.out.println("0");
+        }
+        else {
+            //String[] splitted = s.split("[A-Za-z !,\\?\\._'@]+");
+            //String[] splitted = s.split("[ ',\\?!@\\._]+");
+            String[] splitted = s.split("[^A-Za-z]+");
+            System.out.println(splitted.length);
+            for (String ss: splitted) {
+                System.out.println(ss);
+            }
+        }
         scan.close();
     }
 }

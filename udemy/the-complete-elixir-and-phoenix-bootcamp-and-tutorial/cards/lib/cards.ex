355ec148d898d1b9fcdbdedc343edf4e8f7ae0c4
jepemo <jeremiespm@gmail.com>
2019-01-02 20:27:21 +0100

    elixir course 1

---
+++ udemy/the-complete-elixir-and-phoenix-bootcamp-and-tutorial/cards/lib/cards.ex
@@ -0,0 +1,29 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]    
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)    
+  end
+
+  def save do
+  end
+
+  def load do
+  end
+
+end

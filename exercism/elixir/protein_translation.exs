d4ad7e459714ce308ac1665ab66d3e333f69cfc7
Jeremies Pérez <jeremiespm@gmail.com>
2019-03-01 12:26:05 +0100

    Create protein_translation.exs

---
+++ exercism/elixir/protein_translation.exs
@@ -0,0 +1,79 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    rna
+    |> String.codepoints()
+    |> Enum.chunk_every(3)
+    |> Enum.map(&(Enum.join(&1, "")))
+    |> Enum.reduce([], fn x, acc ->
+      case of_codon(x) do
+        {:ok, "STOP"} -> 
+          case acc do
+            ["STOP" | _] -> acc
+            _ -> ["STOP" | acc]
+          end
+        {:ok, name} -> 
+          case acc do
+            ["STOP" | _] -> acc
+            _ -> acc ++ [name]
+          end
+        {:error, _} -> []
+      end
+    end)
+    |> (fn res ->
+      case res do
+        [] -> {:error, "invalid RNA"}
+        ["STOP" | res] -> {:ok, res}
+        _ -> {:ok, res}
+      end
+    end).()
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _ -> {:error, "invalid codon"}
+    end
+  end
+end

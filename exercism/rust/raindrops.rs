e652d06410d0d3f7e5f5f928c8e88ea22afd2757
Jeremies Pérez <jeremiespm@gmail.com>
2019-09-05 12:49:21 +0200

    Create raindrops.rs

---
+++ exercism/rust/raindrops.rs
@@ -0,0 +1,13 @@
+pub fn raindrops(n: u32) -> String {
+    let mut result = String::new();
+
+    if n % 3 == 0 { result.push_str("Pling") }
+    if n % 5 == 0 { result.push_str("Plang") }
+    if n % 7 == 0 { result.push_str("Plong") }
+
+    if result.is_empty() {
+        result = n.to_string();
+    }
+    
+    result
+}

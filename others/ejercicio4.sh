39ee296a1d1d41cf56840ad793ea4d3960d8511d
Jeremies Pérez <jeremiespm@gmail.com>
2019-11-08 10:15:22 +0100

    xxxx

--- others/ejercicio4.sh
+++ others/ejercicio4.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# -------------------------------------------------
+# Herramiento que muestra y almacena información de uso de los usuarios.
+#
+# - Almacena en el directorio "/root/informes" informacion historica de las generaciones.
+# - El formato de los ficheros es: usuario-anyo-mes-dia-hora
+# - En cada fichero puede almacenar dos tipos de informacion:
+#   - Ficheros modificador por el usuario en ficho dia/hora (prefijo FICHERO_MODIFICADO)
+#   - Tiempo de duracion de la conexion en la hora (prefijo TIEMPO_SESSION_USUARIO)
+# - La herramienta tiene que ejecutarse con permisos de root, además tiene que estar instalada la herramienta ac.
+# - Los parametros de entrada son:
+#   -a: Extrae informacion ficheros modificados
+#   -e: Extrae informacion conexiones
+#   -u <NombreUsuario>: Realiza la tarea para un usuario concreto
+# - Si no se marca ninguna opción por defecto NO realiza ninguna de las operaciones.
+# -------------------------------------------------
 
 ROOT_UID=0
 E_NOTROOT=87
@@ -18,18 +33,18 @@ usage()
 
 preconditions()
 {
-# Se comprueba si el usuario  tiene permisos de ROOT
-if [ "$EUID" -ne 0 ]; then 
-	echo "Se necesitan permisos de root para ejecutar este script"
-	exit $E_NOTROOT
-fi
+	# Se comprueba si el usuario  tiene permisos de ROOT
+	if [ "$EUID" -ne 0 ]; then 
+		echo "Se necesitan permisos de root para ejecutar este script"
+		exit $E_NOTROOT
+	fi
 
-# Comprobamos si tenemos las utilizadades que necesitamos
-which_ex=$(which ac)
-if [ -z "$which_ex" ]; then
-	echo "La aplicacion ac debe estar instalada."
-	exit $E_NOTWHICH
-fi
+	# Comprobamos si tenemos las utilizadades que necesitamos
+	which_ex=$(which ac)
+	if [ -z "$which_ex" ]; then
+		echo "La aplicacion ac debe estar instalada."
+		exit $E_NOTWHICH
+	fi
 }
 
 # Comprobaciones para ver si se puede ejecutar el script
@@ -45,7 +60,7 @@ while getopts "aeu:" flag; do
 			;;
 		u)
 			FLAG_SEL_USER=${OPTARG}
-			# Falta comprobar si el usuario existe
+			# TODO: Comprobar si el usuario existe, sino salir
 			;;
 		*)
 			usage
@@ -70,22 +85,16 @@ touch $PATH_MODIFIED_FILES
 
 # Almacenamos en un fichero el listado de los ficheros de todos los usuarios
 if [ ! -z $FLAG_STORE_FILES ]; then
-	#current_secs=$(date +%s)
-	#day_ago_secs=`expr $current_secs - 86400`
-	#echo $day_ago_secs
-	#sudo find /home/mgr/tmp/ -exec stat --format '%Y %n' "{}" \; \
-	#	| awk -v secs="$day_ago_secs" '{ if($1 > secs) { print $2; } }' > $PATH_MODIFIED_FILES
 	sudo find /home/mgr/tmp/ -mtime -1 -exec ls -ltr --time-style=full-iso {} + > $PATH_MODIFIED_FILES
 fi
 
 for user in $FLAG_SEL_USER
 do
-	#echo -ne "$user\n"
+	echo -ne "=> Procesando usuario: $user\n"
 	if [ ! -z $FLAG_STORE_FILES ]; then
+		echo "==> Extrayendo informacion ficheros modificados"
+		
 		tmp_user_file=/tmp/${user}_tmp
-		echo $tmp_user_file
-
-		#cat $PATH_MODIFIED_FILES
 
 		grep "$user" $PATH_MODIFIED_FILES \
 			| awk '{print $6 " " $7 " " $9}' \
@@ -95,7 +104,8 @@ do
 		days=$(cat $tmp_user_file | awk '{print $1}' | sort | uniq)
 		hours=$(cat $tmp_user_file | awk '{print $2}' | sort | uniq)
 
-		echo "Ficheros modificados por $user"
+		# Mostramos la salida por pantalla
+		echo "===> Ficheros modificados por $user"
 		cat $tmp_user_file
 
 		for day in $days
@@ -103,7 +113,6 @@ do
 			for hour in $hours
 			do
 				file=${PATH_INFORMES}/${user}-${day}-${hour}
-				#echo $file
 
 				ptt="$day_$hour"
 				grep $ptt $tmp_user_file \
@@ -111,11 +120,56 @@ do
 			done
 		done
 
-		#echo $days
-		#echo $hours
-
 		rm -f $tmp_user_file
+	fi
+	
+	if [ ! -z $FLAG_STORE_CONNS ]; then
+		echo "==> Extrayendo informacion sessiones"
+		tmp_user_file1=/tmp/${user}_tmp1
+		tmp_user_file2=/tmp/${user}_tmp2
+		tmp_user_file3=/tmp/${user}_tmp3
+		
+		# Extraemos las diferentes fechas
+		ac -day $user | head -n -1 | awk '{print "date -d\""$1FS$2FS$3"\" +%Y-%m-%d"}' | bash > $tmp_user_file1
+		ac -day $user | head -n -1 | awk '{print $5}' > $tmp_user_file2
+		
+		paste $tmp_user_file1 $tmp_user_file2 > $tmp_user_file3
+		
+		# Mostramos la salida por pantalla
+		echo "===> Conexiones del usuario"
+		cat $tmp_user_file3
+		
+		fechas=$(cat $tmp_user_file3 | sort | uniq | awk '{print $1}')
+		for fecha in $fechas
+		do
+			file=${PATH_INFORMES}/${user}-${fecha}-00
+			grep $fecha $tmp_user_file3 | awk '{print "TIEMPO_SESSION_USUARIO:"$2}' >> $file
+		done
+		
+		rm -f $tmp_user_file1
+		rm -f $tmp_user_file2
+		rm -f $tmp_user_file3
 	fi
 done
 
 rm -f $PATH_MODIFIED_FILES
+
+# Compactacion de los ficheros, para evitar duplicados
+echo "==> Compactando ficheros generados"
+FILES=${PATH_INFORMES}/*
+for filepath in $FILES
+do
+	#filepath=${PATH_INFORMES}/$filename
+	tmp_file=/tmp/informe_out_tmp
+	
+	# Reordenamos y quitamos duplicados
+	cat $filepath | sort | uniq > $tmp_file
+	
+	# sustituimos el fichero de entrada por la salida limpia
+	cat $tmp_file > $filepath
+	
+	# Borramos el fichero temporal
+	rm -f $tmp_file
+done 
+
+echo "=> Ficheros historicos actualizados en $PATH_INFORMES"

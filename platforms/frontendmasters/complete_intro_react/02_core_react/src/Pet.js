f7e5740e3f29a318460374e78841327c66a6b79f
jepemo <jeremiespm@gmail.com>
2021-10-10 12:13:57 +0200

    create custom hook2

--- platforms/frontendmasters/complete_intro_react/02_core_react/src/Pet.js
+++ platforms/frontendmasters/complete_intro_react/02_core_react/src/Pet.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-// export default function Pet({ name, animal, breed }) {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, name),
-//     React.createElement("h2", {}, animal),
-//     React.createElement("h2", {}, breed),
-//   ]);
-// }
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
 
-const Pet = (props) => {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.animal}</h3>
-      <h3>{props.breed}</h3>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
--- platforms/frontendmasters/complete_intro_react/02_core_react/src/SearchParams.js
+++ platforms/frontendmasters/complete_intro_react/02_core_react/src/SearchParams.js
@@ -1,28 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
 
-const ANIMALS = Object.freeze([
-  "bird",
-  "dog",
-  "cat",
-  "rabbit",
-  "reptile",
-  "raven",
-]);
+const ANIMALS = Object.freeze(["bird", "dog", "cat", "rabbit", "reptile"]);
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
 
-  const breeds = [];
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label html-for="location">
           Location
           <input
             id="location"
+            onCut={console.log("Cut!")}
             onChange={(e) => setLocation(e.target.value)}
             value={location}
             placeholder="Location"
@@ -65,6 +80,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
